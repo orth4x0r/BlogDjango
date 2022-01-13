34827b588c52a49dada39fb1199d126918866447
Gabriela Durán Muñoz <gabydm15@gmail.com>
2022-01-12 20:25:38 -0600

    Se agregaron nueva páginas con link a cada una desde el menu, envio de formularios de posts

---
+++ BlogDjango/AppPosts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+import datetime
+
+class NewPost(forms.Form):
+    titulo = forms.CharField(required=True)
+    fecha = forms.DateField(initial=datetime.date.today)
+    texto = forms.CharField(required=True)
--- BlogDjango/AppPosts/views.py
+++ BlogDjango/AppPosts/views.py
@@ -2,10 +2,32 @@ from django.shortcuts import render
 from django.template import Context, Template, loader
 from django.http import HttpResponse
 from datetime import datetime
+from AppPosts.models import *
+from AppPosts.forms import *
 
-def Intro(request):
- 
+def nosotrxs(request):
     return render(request, 'AppPosts/nosotrxs.html')
 
-def BlogPosts(request):
-    return render(request, 'AppPosts/tus_posts.html')
+def misPosts(request):
+    return render(request, 'AppPosts/mis_posts.html')
+
+def bienvenida(request):
+    return render(request, 'AppPosts/bienvenida.html')
+
+def nuevoPost(request):
+
+    if request.method == "POST":
+
+        nuevoPost = NewPost(request.POST)
+
+        if nuevoPost.is_valid():
+            informacion = nuevoPost.cleaned_data
+            newPost = Post(titulo=informacion["titulo"],fecha=informacion["fecha"],texto=informacion["texto"])
+            newPost.save()
+
+        return render(request, 'AppPosts/mis_posts.html')
+
+    else:
+        nuevoPost = NewPost()
+    return render(request, 'AppPosts/nuevo_post.html', {"nuevoPost":nuevoPost})
+
